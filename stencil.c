344d1f872d9a8e5ae84c5a1ff3fa34582e997485
tgmattso <timothy.g.mattson@intel.com>
2015-10-23 15:59:10 -0700

    updated for an error in initializing the weights

--- stencil.c
+++ stencil.c
@@ -1,35 +1,3 @@
-/*
-Copyright (c) 2013, Intel Corporation
-
-Redistribution and use in source and binary forms, with or without 
-modification, are permitted provided that the following conditions 
-are met:
-
-* Redistributions of source code must retain the above copyright 
-      notice, this list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above 
-      copyright notice, this list of conditions and the following 
-      disclaimer in the documentation and/or other materials provided 
-      with the distribution.
-* Neither the name of Intel Corporation nor the names of its 
-      contributors may be used to endorse or promote products 
-      derived from this software without specific prior written 
-      permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
-FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
-COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
-LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
-ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-POSSIBILITY OF SUCH DAMAGE.
-*/
-
 /*******************************************************************
 
 NAME:    Stencil
@@ -46,199 +14,160 @@ USAGE:   The program takes as input the linear
          The output consists of diagnostics to make sure the 
          algorithm worked, and of timing statistics.
 
-FUNCTIONS CALLED:
+HISTORY: This program is based on the stencil program from the Parallel
+         Reserach kernels.  To learn more, go to
 
-         Other than standard C functions, the following functions are used in 
-         this program:
-         omp_get_wtime()
-
-HISTORY: - Written by Rob Van der Wijngaart, February 2009.
-         - RvdW: Removed unrolling pragmas for clarity;
-           added constant to array "in" at end of each iteration to force 
-           refreshing of neighbor data in parallel versions; August 2013
+             https://github.com/ParRes/Kernels
   
 **********************************************************************************/
 
-#include "stencil.h"
-#include <omp.h>
-
-#ifndef RADIUS
-  #define RADIUS 8
+#include <stdio.h>
+#ifdef APPLE
+#include <stdlib.h>
 #endif
+#include <sys/time.h>
+#define  USEC_TO_SEC  1.0e-6    /* to convert microsecs to secs */
 
-#ifdef DOUBLE
-  #define DTYPE   double
-  #define EPSILON 1.e-8
-  #define COEFX   1.0
-  #define COEFY   1.0
-  #define FSTR    "%lf"
-#else
-  #define DTYPE   float
-  #define EPSILON 0.0001f
-  #define COEFX   1.0f
-  #define COEFY   1.0f
-  #define FSTR    "%f"
-#endif
+#define DEF_SIZE      2000
+#define RADIUS        8
+//#define RESTRICT    restrict
+#define RESTRICT
+#define EPSILON       1.0e-8
+#define COEFX         1.0
+#define COEFY         1.0
+#define EXIT_FAIL     66
+#define EXIT_SUCCESS  0  
 
 /* define shorthand for indexing a multi-dimensional array                       */
 #define IN(i,j)       in[i+(j)*(n)]
 #define OUT(i,j)      out[i+(j)*(n)]
 #define WEIGHT(ii,jj) weight[ii+RADIUS][jj+RADIUS]
+#define ABS(x)        ((x)>0 ? (x) : (-(x)))
+
+double wtime() {
+  double time_seconds;
+  struct timeval  time_data; /* seconds since 0 GMT */
+   
+  gettimeofday(&time_data,NULL);
+  time_seconds  = (double) time_data.tv_sec;
+  time_seconds += (double) time_data.tv_usec * USEC_TO_SEC;
+
+  return time_seconds;
+}
 
 int main(int argc, char ** argv) {
 
-  long   n;               /* linear grid dimension                               */
-  int    tile_size;       /* loop nest block factor                              */
-  int    tiling=0;        /* boolean indication loop nest blocking               */
-  int    i, j, ii, jj, it, jt, iter;  /* dummies                                 */
-  DTYPE  norm,            /* L1 norm of solution                                 */
+  long   n;                 /* linear grid dimension                               */
+  int    i, j, ii, jj, iter;/* dummies                                             */
+  double norm = 0.0,        /* L1 norm of solution                                 */
          reference_norm;
-  DTYPE  f_active_points; /* interior of grid with respect to stencil            */
-  DTYPE  flops;           /* floating point ops per iteration                    */
-  int    iterations;      /* number of times to run the algorithm                */
+  double f_active_points; /* interior of grid with respect to stencil            */
+  double flops;           /* floating point ops per iteration                    */
+  int    iterations=25;   /* number of times to run the algorithm                */
   double stencil_time,    /* timing parameters                                   */
          avgtime;
   int    stencil_size;    /* number of points in stencil                         */
-  DTYPE  * RESTRICT in;   /* input grid values                                   */
-  DTYPE  * RESTRICT out;  /* output grid values                                  */
+  double  * RESTRICT in;  /* input grid values                                   */
+  double  * RESTRICT out; /* output grid values                                  */
   long   total_length;    /* total required length to store grid values          */
-  DTYPE  weight[2*RADIUS+1][2*RADIUS+1]; /* weights of points in the stencil     */
+  double weight[2*RADIUS+1][2*RADIUS+1]; /* weights of points in the stencil     */
 
-  /*******************************************************************************
-  ** Hardware input parameters    
-  ********************************************************************************/
-  n          = 2000;
-  iterations = 100;
-  tile_size  = 1;
-
-  if (RADIUS < 1) {
-    printf("ERROR: Stencil radius %d should be positive\n", RADIUS);
-    exit(EXIT_FAILURE);
-  }
+   if(argc ==2){
+      n = atoi(argv[1]);
+   }
+   else{
+      n = DEF_SIZE;
+   }
 
   if (2*RADIUS +1 > n) {
     printf("ERROR: Stencil radius %d exceeds grid size %d\n", RADIUS, n);
-    exit(EXIT_FAILURE);
+    exit(EXIT_FAIL);
   }
 
-  /*  make sure the vector space can be represented                             */
-  total_length = n*n*sizeof(DTYPE);
-  if (total_length/n != n*sizeof(DTYPE)) {
-    printf("ERROR: Space for %d x %d grid cannot be represented; ", n, n);
-    exit(EXIT_FAILURE);
-  }
-
-  in  = (DTYPE *) malloc(total_length);
-  out = (DTYPE *) malloc(total_length);
+  /*  allocate the required space                                               */
+  total_length = n*n*sizeof(double);
+  in  = (double *) malloc(total_length);
+  out = (double *) malloc(total_length);
   if (!in || !out) {
     printf("ERROR: could not allocate space for input or output array\n");
-    exit(EXIT_FAILURE);
+    exit(EXIT_FAIL);
   }
 
   /* fill the stencil weights to reflect a discrete divergence operator         */
-  for (jj=-RADIUS; jj<=RADIUS; jj++) for (ii=-RADIUS; ii<=RADIUS; ii++)
-    WEIGHT(ii,jj) = (DTYPE) 0.0;
   stencil_size = (2*RADIUS+1)*(2*RADIUS+1);
+  for (jj=-RADIUS; jj<= RADIUS; jj++) for (ii=-RADIUS; ii<= RADIUS; ii++)
+    WEIGHT(ii,jj)=0.0;
   for (jj=1; jj<=RADIUS; jj++) {
     for (ii=-jj+1; ii<jj; ii++) {
-      WEIGHT(ii,jj)  =  (DTYPE) (1.0/(4.0*jj*(2.0*jj-1)*RADIUS));
-      WEIGHT(ii,-jj) = -(DTYPE) (1.0/(4.0*jj*(2.0*jj-1)*RADIUS));
-      WEIGHT(jj,ii)  =  (DTYPE) (1.0/(4.0*jj*(2.0*jj-1)*RADIUS));
-      WEIGHT(-jj,ii) = -(DTYPE) (1.0/(4.0*jj*(2.0*jj-1)*RADIUS));      
+      WEIGHT(ii,jj)  =  (double) (1.0/(4.0*jj*(2.0*jj-1)*RADIUS));
+      WEIGHT(ii,-jj) = -(double) (1.0/(4.0*jj*(2.0*jj-1)*RADIUS));
+      WEIGHT(jj,ii)  =  (double) (1.0/(4.0*jj*(2.0*jj-1)*RADIUS));
+      WEIGHT(-jj,ii) = -(double) (1.0/(4.0*jj*(2.0*jj-1)*RADIUS));      
     }
-    WEIGHT(jj,jj)    =  (DTYPE) (1.0/(4.0*jj*RADIUS));
-    WEIGHT(-jj,-jj)  = -(DTYPE) (1.0/(4.0*jj*RADIUS));
+    WEIGHT(jj,jj)    =  (double) (1.0/(4.0*jj*RADIUS));
+    WEIGHT(-jj,-jj)  = -(double) (1.0/(4.0*jj*RADIUS));
   }
 
-  norm = (DTYPE) 0.0;
-  f_active_points = (DTYPE) (n-2*RADIUS)*(DTYPE) (n-2*RADIUS);
+  f_active_points = (double) (n-2*RADIUS)*(double) (n-2*RADIUS);
 
-  printf("Parallel Research Kernels Version %s\n", PRKVERSION);
   printf("Serial stencil execution on 2D grid\n");
   printf("Grid size            = %d\n", n);
   printf("Radius of stencil    = %d\n", RADIUS);
   printf("Type of stencil      = compact\n");
-#ifdef DOUBLE
-  printf("Data type            = double precision\n");
-#else
-  printf("Data type            = single precision\n");
-#endif
-  if (tiling) printf("Tile size            = %d\n", tile_size);
-  else        printf("Untiled\n");
   printf("Number of iterations = %d\n", iterations);
 
   /* intialize the input and output arrays                                     */
   for (j=0; j<n; j++) for (i=0; i<n; i++) 
     IN(i,j) = COEFX*i+COEFY*j;
   for (j=RADIUS; j<n-RADIUS; j++) for (i=RADIUS; i<n-RADIUS; i++) 
-    OUT(i,j) = (DTYPE)0.0;
+    OUT(i,j) = 0.0;
 
   for (iter = 0; iter<=iterations; iter++){
 
     /* start timer after a warmup iteration */
-    if (iter == 1)  stencil_time = omp_get_wtime();
+    if (iter == 1)  stencil_time = wtime();
 
-    /* Apply the stencil operator                                              */
-    if (!tiling) {
-      for (j=RADIUS; j<n-RADIUS; j++) {
-        for (i=RADIUS; i<n-RADIUS; i++) {
-          /* would like to be able to unroll this loop, but compiler will ignore  */
-          for (jj=-RADIUS; jj<=RADIUS; jj++) 
-          for (ii=-RADIUS; ii<=RADIUS; ii++)  OUT(i,j) += WEIGHT(ii,jj)*IN(i+ii,j+jj);
-        }
-      }
-    }
-    else {
-      for (j=RADIUS; j<n-RADIUS; j+=tile_size) {
-        for (i=RADIUS; i<n-RADIUS; i+=tile_size) {
-          for (jt=j; jt<MIN(n-RADIUS,j+tile_size); jt++) {
-            for (it=i; it<MIN(n-RADIUS,i+tile_size); it++) {
-              /* would like to be able to unroll this loop, but compiler will ignore  */
-              for (jj=-RADIUS; jj<=RADIUS; jj++) 
-              for (ii=-RADIUS; ii<=RADIUS; ii++)  OUT(it,jt) += WEIGHT(ii,jj)*IN(it+ii,jt+jj);
-            }
-          }
-        }
+    /* Apply the stencil operator                                               */
+    for (j=RADIUS; j<n-RADIUS; j++) {
+      for (i=RADIUS; i<n-RADIUS; i++) {
+        /* would like to be able to unroll this loop, but compiler will ignore  */
+        for (jj=-RADIUS; jj<=RADIUS; jj++) 
+        for (ii=-RADIUS; ii<=RADIUS; ii++)  OUT(i,j) += WEIGHT(ii,jj)*IN(i+ii,j+jj);
       }
     }
 
-
-    /* add constant to solution to force refresh of neighbor data, if any       */
+    /* add constant to solution to force refresh of input data                  */
     for (j=0; j<n; j++) for (i=0; i<n; i++) IN(i,j)+= 1.0;
 
   } /* end of iterations                                                        */
 
-  stencil_time = omp_get_wtime() - stencil_time;
+  stencil_time = wtime() - stencil_time;
 
-  /* compute L1 norm in parallel                                                */
+  /* compute L1 norm                                                            */
   for (j=RADIUS; j<n-RADIUS; j++) for (i=RADIUS; i<n-RADIUS; i++) {
-    norm += (DTYPE)ABS(OUT(i,j));
+    norm += (double)ABS(OUT(i,j));
   }
 
   norm /= f_active_points;
 
-  /*******************************************************************************
+  /******************************************************************************
   ** Analyze and output results.
-  ********************************************************************************/
+  *******************************************************************************/
 
-/* verify correctness                                                            */
-  reference_norm = (DTYPE) (iterations+1) * (COEFX + COEFY);
+/* verify correctness                                                           */
+  reference_norm = (double) (iterations+1) * (COEFX + COEFY);
   if (ABS(norm-reference_norm) > EPSILON) {
-    printf("ERROR: L1 norm = "FSTR", Reference L1 norm = "FSTR"\n",
+    printf("ERROR: L1 norm = %lf, Reference L1 norm = %lf\n",
            norm, reference_norm);
-    exit(EXIT_FAILURE);
+    exit(EXIT_FAIL);
   }
   else {
     printf("Solution validates\n");
-#ifdef VERBOSE
-    printf("Reference L1 norm = "FSTR", L1 norm = "FSTR"\n", 
-           reference_norm, norm);
-#endif
   }
 
-  flops = (DTYPE) (2*stencil_size+1) * f_active_points;
+  flops = (double) (2*stencil_size+1) * f_active_points;
   avgtime = stencil_time/iterations;
-  printf("Rate (MFlops/s): "FSTR"  Avg time (s): %lf\n",
+  printf("Serial Rate (MFlops/s): %lf  Avg time (s): %lf\n",
          1.0E-06 * flops/avgtime, avgtime);
 
   exit(EXIT_SUCCESS);
